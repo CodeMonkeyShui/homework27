443171dbe09a06720d1eb368ff1332ba06e1e167
CodeMonkeyShui <5443974@qq.com>
2018-06-26 21:24:47 +0800

    8

--- bom.js
+++ bom.js
@@ -10,7 +10,7 @@ window.$ = function(){
 
 $.bom = {
 	openAtCenter: function(width, height, url){
-		let st = 'width='+width+'px,height='+height+'}px'
+		let st = 'width='+width+'px,height='+height+'}px,screenX='+(screen.width/2-width/2)+'px,screenY='+(screen.height/2-height/2)+'px'
 		window.open(url, '_blank', st)
 	}
 }
